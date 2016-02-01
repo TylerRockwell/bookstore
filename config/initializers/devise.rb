7d32b29515ea7785b96fffa3012d49cc22332cc6
Tyler Rockwell <tyler.rockwell87@gmail.com>
2016-02-01 13:12:04 -0500

    Add email confirmation to new user accounts

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,7 +7,7 @@ Devise.setup do |config|
   config.strip_whitespace_keys = [:email]
   config.skip_session_storage = [:http_auth]
   config.stretches = Rails.env.test? ? 1 : 10
-  config.reconfirmable = true
+  config.reconfirmable = false
   config.expire_all_remember_me_on_sign_out = true
   config.password_length = 8..72
   config.reset_password_within = 6.hours
