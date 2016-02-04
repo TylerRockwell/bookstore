081f1632e9ef56321b6ad7b2460d66de4c2ac44e
Tyler Rockwell <tyler.rockwell87@gmail.com>
2016-02-04 13:51:48 -0500

    Simplify cart spec

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -5,21 +5,14 @@ RSpec.describe Cart, type: :model do
   it { is_expected.to have_many(:line_items) }
 
   describe "#calculate_total" do
-    let(:cart) { create(:cart) }
-    let(:item_1) { create(:line_item, cart: cart) }
-    let(:item_2) { create(:line_item, cart: cart) }
+    let(:cart)    { create(:cart) }
+    let(:book_1)  { create(:book, price: 10) }
+    let(:book_2)  { create(:book, price: 20) }
+    let!(:item_1) { create(:line_item, cart: cart, book: book_1) }
+    let!(:item_2) { create(:line_item, cart: cart, book: book_2) }
     it "calculates the total price for the order" do
-      cart.line_items << item_1
-      cart.line_items << item_2
-      expect(cart.calculate_total).to eq(item_1.total_price + item_2.total_price)
-    end
-
-    it "calculates the total price for the order" do
-      item_1.book.price = 10
-      item_2.book.price = 20
-      cart.line_items << item_1
-      cart.line_items << item_2
-      expect(cart.calculate_total).to eq(30)
+      expect(cart.reload.calculate_total).to eq(item_1.total_price + item_2.total_price)
+      expect(cart.reload.calculate_total).to eq(30)
     end
   end
 end
