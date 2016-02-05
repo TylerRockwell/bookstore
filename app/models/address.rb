fac0dc0c6abf3d00f37965e951cc3484ab8bd8eb
Tyler Rockwell <tyler.rockwell87@gmail.com>
2016-02-05 11:07:25 -0500

    Add address model

---
+++ app/models/address.rb
@@ -0,0 +1,7 @@
+class Address < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :order
+
+  validates :street_number, :street_name, :city, presence: true
+  validates :state, :zip, :order, :user, presence: true
+end
---
+++ spec/models/address_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Address, type: :model do
+  describe "associations" do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:order) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:street_number) }
+    it { is_expected.to validate_presence_of(:street_name) }
+    it { is_expected.to validate_presence_of(:city) }
+    it { is_expected.to validate_presence_of(:state) }
+    it { is_expected.to validate_presence_of(:zip) }
+    it { is_expected.to validate_presence_of(:user) }
+    it { is_expected.to validate_presence_of(:order) }
+  end
+end
