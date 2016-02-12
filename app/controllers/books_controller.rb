f9501e83f4d42040126504c2c70a0d58d8329cf6
Tyler Rockwell <tyler.rockwell87@gmail.com>
2016-02-12 16:46:11 -0500

    Reimplement manual search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
                        .page(params[:page])
 
     else
-      @books = Book.ransack(params[:search]).result
+      @books = Book.search(params[:search])
                    .order_by(params[:sort_field], params[:sort_order])
                    .page(params[:page])
                    .decorate
